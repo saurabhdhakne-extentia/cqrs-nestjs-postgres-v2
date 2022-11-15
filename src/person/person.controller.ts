daea58011ed0892dd856b66c030cce50cff318a3
Saurabh Dhakne <saurabh.dhakne@extentia.com>
2022-11-15 18:22:44 +0530

    API to delete person

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, HttpCode, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Patch } from '@nestjs/common/decorators';
+import { Body, Controller, Get, HttpCode, Post, Delete, Patch, UsePipes, ValidationPipe } from '@nestjs/common';
 import { QueryBus, CommandBus } from '@nestjs/cqrs';
+import { DeletePersonCommand } from './commands/impl/delete-person.command';
 import { SavePersonCommand } from './commands/impl/save-person.command';
 import { UpdatePersonCommand } from './commands/impl/update-person.command';
 import { GetPersonsQuery } from './queries/impl/get-persons.query';
@@ -25,6 +25,13 @@ export class PersonController {
     @HttpCode(201)
     @UsePipes(new ValidationPipe({ transform: true }))
     async updateEmployee(@Body() updatedPerson: UpdatePersonCommand) {
+        return await this.commandBus.execute(updatedPerson);
+    }
+
+    @Delete('delete')
+    @HttpCode(201)
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async deleteEmployee(@Body() updatedPerson: DeletePersonCommand) {
         return await this.commandBus.execute(updatedPerson);
     }
 
